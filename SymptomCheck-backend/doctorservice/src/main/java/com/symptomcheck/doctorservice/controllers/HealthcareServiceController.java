a61d5f10fa434261880ede1d5f49f4a555b313b2
jlassi mazen <jlmazen1073@gmail.com>
2025-11-20 16:22:26 +0100

    unit test for healthCareServiceService

--- SymptomCheck-backend/doctorservice/src/main/java/com/symptomcheck/doctorservice/controllers/HealthcareServiceController.java
+++ SymptomCheck-backend/doctorservice/src/main/java/com/symptomcheck/doctorservice/controllers/HealthcareServiceController.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @RestController()
 @RequestMapping("/api/v1/doctor/healthcare/service")
 @RequiredArgsConstructor
+
 public class HealthcareServiceController {
     private final HealthcareServiceService healthcareService;
 
