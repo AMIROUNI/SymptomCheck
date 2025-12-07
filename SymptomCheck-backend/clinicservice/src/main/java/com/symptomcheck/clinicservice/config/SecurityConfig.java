186c999c3308153a0c845b0f458df7c03480e6c2
amir ouni <amirouni162@gmail.com>
2025-12-07 21:26:02 +0100

    try to test the api health  to fix the  readness prob and the  liveness prob

--- SymptomCheck-backend/clinicservice/src/main/java/com/symptomcheck/clinicservice/config/SecurityConfig.java
+++ SymptomCheck-backend/clinicservice/src/main/java/com/symptomcheck/clinicservice/config/SecurityConfig.java
@@ -60,6 +60,7 @@ public class SecurityConfig {
                                 "/api/v1/users/public/**",
                                 "/api/v1/auth/**",
                                 "/actuator/health",
+                                "/actuator/health/**",
                                 "/uploads/**",
                                 "/api/v1/medical/clinic"
                         ).permitAll()
